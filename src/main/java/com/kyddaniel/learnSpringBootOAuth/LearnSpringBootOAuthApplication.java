62f7ceb217c96af07dd42ab0f35f02b3714c47c1
kyddaniel <mksuenaa@connect.ust.hk>
2024-12-08 20:53:48 +0800

    Initial commit

---
+++ src/main/java/com/kyddaniel/learnSpringBootOAuth/LearnSpringBootOAuthApplication.java
@@ -0,0 +1,13 @@
+package com.kyddaniel.learnSpringBootOAuth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSpringBootOAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnSpringBootOAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kyddaniel/learnSpringBootOAuth/LearnSpringBootOAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kyddaniel.learnSpringBootOAuth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSpringBootOAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
